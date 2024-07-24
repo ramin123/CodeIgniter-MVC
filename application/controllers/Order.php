ae9d9a267c84b25989de68adf6e8357b21f0f275
ramin123 <109870123+ramin123@users.noreply.github.com>
2024-07-24 12:54:04 +0700

    Update Order.php

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -1,21 +1,5 @@
 <?php 
-/**
- * Restaurant-Conquerors Market
- * 
- * An online food order system in codeigniter framework
- * 
- * This content is released under the Codecanyon Market License (CML)
- * 
- * Copyright (c) 2018 - 2019, Conquerors Market
- *
- * @category  Order
- * @package   Restaurant
- * @author    Conquerors Market <conquerorsmarket@gmail.com>
- * @copyright 2018 - 2019, Conquerors Market
- * @license   http://opensource.org/licenses/MIT    MIT License
- * @link      http://codeigniter.com
- * @since     Version 1.0.0
- */
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
