140ad5ba1faa12e2ee749cf342a50fc45c2146a2
ramin123 <109870123+ramin123@users.noreply.github.com>
2024-07-24 12:57:47 +0700

    Update User_authentication_facebook.php

--- application/controllers/User_authentication_facebook.php
+++ application/controllers/User_authentication_facebook.php
@@ -1,56 +1,11 @@
 <?php 
-/**
- * Restaurant-Conquerors Market
- * 
- * An online food order system in codeigniter framework
- * 
- * This content is released under the Codecanyon Market License (CML)
- * 
- * Copyright (c) 2018 - 2019, Conquerors Market
- *
- * @category  Userauthenticationfacebook
- * @package   Restaurant
- * @author    Conquerors Market <conquerorsmarket@gmail.com>
- * @copyright 2018 - 2019, Conquerors Market
- * @license   http://opensource.org/licenses/MIT    MIT License
- * @link      http://codeigniter.com
- * @since     Version 1.0.0
- */
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/**
- * CodeIgniter User_authentication_facebook Class
- * 
- * User_authentication_facebook.
- *
- * @category  Userauthenticationfacebook
- * @package   Restaurant
- * @author    Conquerors Market <conquerorsmarket@gmail.com>
- * @copyright 2018 - 2019, Conquerors Market
- * @license   http://opensource.org/licenses/MIT    MIT License
- * @link      http://codeigniter.com
- */
+
 class User_authentication_facebook extends MY_Controller
 {
-    /**
-    | -----------------------------------------------------
-    | PRODUCT NAME:     RESTAURANT
-    | -----------------------------------------------------
-    | AUTHOR:           CONQUERORS MARKET
-    | -----------------------------------------------------
-    | EMAIL:            conquerorsmarket@gmail.com
-    | -----------------------------------------------------
-    | COPYRIGHTS:       RESERVED BY CONQUERORS MARKET
-    | -----------------------------------------------------      
-    | http://codecanyon.net/user/conquerorsmarket
-    | http://conquerorstech.net/
-    | -----------------------------------------------------
-    |
-    | MODULE:           User_authentication_facebook CONTROLLER
-    | -----------------------------------------------------
-    | This is User_authentication_facebook module controller file.
-    | -----------------------------------------------------
-     **/
+
     function __construct() 
     {
         parent::__construct();
